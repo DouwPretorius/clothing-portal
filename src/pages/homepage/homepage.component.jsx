f28b1eb8145d58379ccf5430454b7d075a8c30e1
Douw Pretorius <douw.pretorius@entelect.co.za>
2019-12-11 17:11:26 +0200

    broken the home page into smaller components, and styled

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
+import Directory from '../../components/directory/directory.component'
+
 const HomePage = () => (
     <div className='homepage'>
-        <h2>Hello!</h2>
+        <Directory />
     </div>
 )
 
